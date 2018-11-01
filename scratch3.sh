4af3ca48d6e0b17494da94ff3824f12533420be6
DallasCarraher <dall133android@gmail.com>
2018-11-01 14:49:57 -0700

    more conditionals

--- scratch3.sh
+++ scratch3.sh
@@ -7,16 +7,41 @@ echo "Hello $name"
 
 #conditionals
 
+#comparisons -> eq ne le lt ge gt
+
 read -p "How old are you? " age
 
 if [ $age -ge 16 ]
 then 
-	echo"You can drive"
+	echo "You can drive"
 elif [$age -eq 15 ]
 then
 	echo "You can drive with a guardian"
 else
 	echo "You can't drive!"
+fi
 
 
+read -p "Enter a  number: " num
+
+if  ((num == 10)); then
+	echo "Your number equals 10"
+fi
+
+if ((num > 10)); then
+	echo "Your number is greater than 10"
+else
+	echo "Your number is less than 10"
+fi
+
+if (( ((num % 2)) == 0 )); then
+	echo "Your number is even"
+else
+	echo "Your number is odd"
+fi
+
+if (( ((num > 0)) && ((num < 11)) ));
+then
+	echo "$num is between 1 and 10"
+fi
 
